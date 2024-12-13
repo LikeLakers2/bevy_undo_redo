5a201aafc55166cd0fe581c66c5b8792910819b8
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2024-12-12 23:42:22 -0500

    I forgot that Godot's UndoRedo is split into Actions, which themselves are split into Operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,40 @@
+use std::any::Any;
+
 use bevy_ecs::system::Resource;
 
 use crate::operation::Operation;
 
 pub mod operation;
 
-#[derive(Clone, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord, Resource)]
-pub struct UndoRedo<O: Operation> {
-	/// The list of stored operations that either have already been taken, or will be taken once
-	/// committed.
-	/// 
-	/// TODO: This list should probably have a limit somewhere.
-	op_list: Vec<O>,
-	/// Where we are in `op_list`. Operations that have already been applied will be before this
-	/// index, and operations that have yet to be applied will be after.
-	cursor_pos: usize,
+#[derive(Default, Resource)]
+pub struct UndoRedo {
+	past_actions: Vec<Action>,
+	queued_actions: Vec<Action>,
+}
+
+impl UndoRedo {
+	pub fn create_action(&mut self, name: String) -> &mut Action {
+		let new_action = Action {
+			name,
+			op_list: vec![],
+		};
+
+		self.queued_actions.push(new_action);
+
+		self.queued_actions
+			.last_mut()
+			.expect("queued action list should not be empty, as we just pushed an item")
+	}
+}
+
+pub struct Action {
+	name: String,
+	/// The list of operations this action takes.
+	op_list: Vec<Box<dyn Operation>>,
+}
+
+impl Action {
+	pub fn push<O: Operation>(&mut self, operation: O) {
+		self.op_list.push(Box::new(operation))
+	}
 }
--- src/operation.rs
+++ src/operation.rs
@@ -1,8 +1,17 @@
-use std::fmt::Debug;
+use std::{any::Any, fmt::Debug};
 
 use bevy_ecs::world::CommandQueue;
 
-pub trait Operation: Debug + Send + Sync + 'static {
+pub trait Operation: Send + Sync + 'static {
 	fn apply(&self) -> CommandQueue;
 	fn undo(&self) -> CommandQueue;
+}
+
+pub struct Details {
+	/// The type of operation that this is; i.e. "Moves an object by a certain amount of pixels"
+	op_type: &'static str,
+
+	/// Any additional info provided by the user. The user is responsible for downcasting this to
+	/// whatever they need.
+	additional_info: Box<dyn Any>,
 }
