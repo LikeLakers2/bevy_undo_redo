18ca546e01d7b524735b833ffd105dcd0e64f8bf
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2024-12-14 15:58:15 -0500

    Thinking about it, this also means that UndoRedo's functions don't need to take a `World` - they can just take a `Commands`

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-use bevy_ecs::{
-	system::{Commands, Resource},
-	world::{CommandQueue, World},
-};
+use bevy_ecs::system::{Commands, Resource};
 
 use crate::{action::Action, operation::Operation};
 
@@ -38,34 +35,25 @@ impl UndoRedo {
 			.expect("queued action list should not be empty, as we just pushed an item")
 	}
 
-	pub fn commit_queued_actions(&mut self, world: &mut World) {
+	pub fn commit_queued_actions(&mut self, commands: &mut Commands) {
 		if self.list_cursor == self.action_list.len() {
 			return;
 		}
-
-		let mut command_queue = CommandQueue::default();
-		let mut commands = Commands::new(&mut command_queue, world);
 
 		let queued_actions = self.action_list.iter().skip(self.list_cursor);
 		self.list_cursor = self.action_list.len();
 
 		for action in queued_actions {
-			action.apply(&mut commands);
+			action.apply(commands);
 		}
-
-		command_queue.apply(world);
 	}
 
-	pub fn redo(&mut self, world: &mut World) {
+	pub fn redo(&mut self, commands: &mut Commands) {
 		// If the cursor is already at the end of `self.action_list`, we have no action to apply. In
 		// this case, we return without doing anything.
 		if self.list_cursor == self.action_list.len() {
 			return;
 		}
-
-		// Otherwise, initialize a CommandQueue and a Commands...
-		let mut command_queue = CommandQueue::default();
-		let mut commands = Commands::new(&mut command_queue, world);
 
 		// Then get the next queued action action...
 		let next_queued_action = self
@@ -74,26 +62,19 @@ impl UndoRedo {
 			.expect("next action should exist");
 
 		// Then have it submit all the commands needed to apply...
-		next_queued_action.apply(&mut commands);
-
-		// And finally, apply those commands to the world.
-		command_queue.apply(world);
+		next_queued_action.apply(commands);
 
 		// Now that we've applied the action, the cursor needs to be updated to point to the next
 		// queued action (or to the end, if none are queued).
 		self.list_cursor += 1;
 	}
 
-	pub fn undo(&mut self, world: &mut World) {
+	pub fn undo(&mut self, commands: &mut Commands) {
 		// If the cursor is already at the beginning of `self.action_list`, we have no action to
 		// undo. In this case, we return without doing anything.
 		if self.list_cursor == 0 {
 			return;
 		}
-
-		// Otherwise, initialize a CommandQueue and a Commands...
-		let mut command_queue = CommandQueue::default();
-		let mut commands = Commands::new(&mut command_queue, world);
 
 		// Then, decrement the list cursor...
 		self.list_cursor -= 1;
@@ -105,9 +86,6 @@ impl UndoRedo {
 			.expect("previous action should exist");
 
 		// Then have it submit all the commands needed to undo...
-		last_committed_action.undo(&mut commands);
-
-		// And finally, apply those commands to the world.
-		command_queue.apply(world);
+		last_committed_action.undo(commands);
 	}
 }
