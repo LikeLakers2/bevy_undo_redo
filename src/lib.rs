fd905fe1d6e97358459a32aa5587585b6dabaf21
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2024-12-16 00:32:38 -0500

    Change action_list to a VecDeque; add feedback on `undo()`, `redo()`, and `apply_queued_actions()`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+use std::{
+	collections::VecDeque,
+	fmt::{Display, Formatter, Result as FmtResult},
+};
+
 use bevy_ecs::system::{Commands, Resource};
 
 use crate::operation::Operation;
@@ -12,7 +17,7 @@ pub struct UndoRedo {
 	/// * A committed action. This action has already been applied to the world, and can be undone.
 	/// * A queued action. This action has been created, and possibly assigned a set of operations,
 	///   but has not been committed to the world.
-	action_list: Vec<Action>,
+	action_list: VecDeque<Action>,
 	/// The first index in `action_list` to be a queued action.
 	///
 	/// Actions before this index are all committed actions, and actions at or after this index are
@@ -27,18 +32,18 @@ impl UndoRedo {
 	pub fn create_queued_action(&mut self, name: String) -> &mut Action {
 		let new_action = Action::new(name);
 
-		self.action_list.push(new_action);
+		self.action_list.push_back(new_action);
 
 		self.action_list
-			.last_mut()
+			.back_mut()
 			.expect("queued action list should not be empty, as we just pushed an item")
 	}
 
 	/// Applies all actions that are queued.
-	pub fn apply_queued_actions(&mut self, commands: &mut Commands) {
+	pub fn apply_queued_actions(&mut self, commands: &mut Commands) -> Result<(), Error> {
 		// If the cursor is already at the end of `self.action_list`, we have no actions to apply.
 		if self.list_cursor == self.action_list.len() {
-			return;
+			return Err(Error::NoActionAvailable);
 		}
 
 		let queued_actions = self.action_list.iter().skip(self.list_cursor);
@@ -48,17 +53,22 @@ impl UndoRedo {
 		}
 
 		self.list_cursor = self.action_list.len();
+
+		Ok(())
 	}
 
 	/// Applies the next queued action, if any. If there are no queued actions, this does nothing.
-	pub fn redo(&mut self, commands: &mut Commands) {
+	pub fn redo(&mut self, commands: &mut Commands) -> Result<(), Error> {
 		// If the cursor is already at the end of `self.action_list`, we have no action to apply. In
-		// this case, we return without doing anything.
+		// this case, we return an error describing as such.
 		if self.list_cursor == self.action_list.len() {
-			return;
+			return Err(Error::NoActionAvailable);
 		}
 
 		// Then get the next queued action action...
+		//
+		// Note: Because we just verified that we're not at the end of the action list, we can
+		// safely assume this will be a `Some(_)` value.
 		let next_queued_action = self
 			.action_list
 			.get(self.list_cursor)
@@ -70,21 +80,26 @@ impl UndoRedo {
 		// Now that we've applied the action, the cursor needs to be updated to point to the next
 		// queued action (or to the end, if none are queued).
 		self.list_cursor += 1;
+
+		Ok(())
 	}
 
 	/// Undoes the last committed action, if any. If there are no committed actions, this does
 	/// nothing.
-	pub fn undo(&mut self, commands: &mut Commands) {
+	pub fn undo(&mut self, commands: &mut Commands) -> Result<(), Error> {
 		// If the cursor is already at the beginning of `self.action_list`, we have no action to
-		// undo. In this case, we return without doing anything.
+		// undo.
 		if self.list_cursor == 0 {
-			return;
+			return Err(Error::NoActionAvailable);
 		}
 
 		// Then, decrement the list cursor...
 		self.list_cursor -= 1;
 
 		// Then, use the new cursor position to get the last committed action...
+		//
+		// Note: Because we just verified that we're not at the beginning of the action list, we can
+		// safely assume this will be a `Some(_)` value.
 		let last_committed_action = self
 			.action_list
 			.get(self.list_cursor)
@@ -92,6 +107,8 @@ impl UndoRedo {
 
 		// Then have it submit all the commands needed to undo...
 		last_committed_action.undo(commands);
+
+		Ok(())
 	}
 }
 
@@ -102,7 +119,7 @@ pub struct Action {
 }
 
 impl Action {
-	pub(crate) fn new(name: String) -> Self {
+	fn new(name: String) -> Self {
 		Self {
 			name,
 			op_list: vec![],
@@ -125,6 +142,21 @@ impl Operation for Action {
 		let reversed_op_list = self.op_list.iter().rev();
 		for op in reversed_op_list {
 			op.undo(commands);
+		}
+	}
+}
+
+#[derive(Debug)]
+pub enum Error {
+	NoActionAvailable,
+}
+
+impl Display for Error {
+	fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+		match self {
+			Self::NoActionAvailable => {
+				write!(f, "No actions are available to perform this operation")
+			}
 		}
 	}
 }
