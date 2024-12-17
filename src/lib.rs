1dbbf4db537a2f17e2ccc64018e9476f5d360a1f
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2024-12-17 03:57:14 -0500

    Move `redo()` and `undo()` into `History`, making them return a reference to the moved item

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub mod operation;
 
 #[derive(Default, Resource)]
 pub struct UndoRedo {
-	history: History,
+	history: History<Box<dyn Operation>>,
 	/// A list of operations that have been pushed to this UndoRedo, but have not been applied to
 	/// the World.
 	queued_operations: VecDeque<Box<dyn Operation>>,
@@ -26,7 +26,7 @@ impl UndoRedo {
 	/// Applies all operations that are queued. Any actions that have been undone, but not
 	/// subsequently redone, will be lost.
 	///
-	/// # Returns
+	/// # Possible errors
 	/// * [`Error::NoWorkAvailable`] - There are no queued operations available to apply.
 	pub fn apply_queued_operations(&mut self, commands: &mut Commands) -> Result<(), Error> {
 		// If there are no queued operations available, we have no work to do. Let the caller know.
@@ -38,7 +38,7 @@ impl UndoRedo {
 
 		for operation in queued_operations {
 			operation.apply(commands);
-			self.history.committed.push_back(operation);
+			self.history.push(operation);
 		}
 
 		self.history.undone.clear();
@@ -47,67 +47,35 @@ impl UndoRedo {
 
 	/// Applies the last undone operation, if any.
 	///
-	/// # Returns
+	/// # Possible errors
 	/// * [`Error::NoApplicableHistory`] - No operations have been undone since the last time (if any)
 	///   queued operations were applied.
 	pub fn redo(&mut self, commands: &mut Commands) -> Result<(), Error> {
-		// If there are no previously-undone operations available, we have no work to do. Let the
-		// caller know.
-		if self.history.undone.is_empty() {
-			return Err(Error::NoApplicableHistory);
-		}
+		let item = self.history.redo()?;
 
-		// Otherwise, get the first undone operation...
-		//
-		// Note: Because we just verified that `!self.history_undone.is_empty()`, we can safely
-		// assume this will be a `Some(_)` value.
-		let last_undone_action = self
-			.history
-			.undone
-			.pop_front()
-			.expect("history_undone should not be empty");
-
-		// Then have it submit all the commands needed to apply...
-		last_undone_action.apply(commands);
-
-		self.history.committed.push_back(last_undone_action);
+		// Submit all the commands needed to apply...
+		item.apply(commands);
 
 		Ok(())
 	}
 
 	/// Undoes the last committed operation, if any.
 	///
-	/// # Returns
+	/// # Possible errors
 	/// * [`Error::NoApplicableHistory`] - There are no operations available to undo.
 	pub fn undo(&mut self, commands: &mut Commands) -> Result<(), Error> {
-		// If there are no operations in the history, we have no work to do. Let the caller know.
-		if self.history.committed.is_empty() {
-			return Err(Error::NoApplicableHistory);
-		}
+		let item = self.history.undo()?;
 
-		// Otherwise, pop an operation off the end of the history...
-		//
-		// NOTE: Because we just verified that `!self.history.is_empty()`, we can safely assume this
-		// will be a `Some(_)` value.
-		let last_committed_action = self
-			.history
-			.committed
-			.pop_back()
-			.expect("history should not be empty");
-
-		// Then have it submit all the commands needed to undo...
-		last_committed_action.undo(commands);
-
-		self.history.undone.push_front(last_committed_action);
+		// Submit all the commands needed to undo...
+		item.undo(commands);
 
 		Ok(())
 	}
 }
 
-#[derive(Default)]
-pub struct History {
+pub struct History<T> {
 	/// A list of all operations that have been committed, in the order they were committed.
-	committed: VecDeque<Box<dyn Operation>>,
+	committed: VecDeque<T>,
 	/// A list of all operations that were committed, but have subsequently been undone. Flushing
 	/// the queued operations list will clear this list.
 	// TODO: Document on the functions that flushing the queued operations list will clear this list
@@ -115,10 +83,99 @@ pub struct History {
 	//
 	// TODO: Maybe combine this into `history` and use a `usize` as a cursor for which ops are
 	// committed and which were undone?
-	undone: VecDeque<Box<dyn Operation>>,
-	/// The maximum length of this history. Any operations past this limit will be automatically
-	/// culled.
+	undone: Vec<T>,
+	/// The maximum length of this history. Any committed operations past this limit will be
+	/// automatically culled.
 	limit: Option<NonZeroUsize>,
+}
+
+impl<T> History<T> {
+	pub fn push(&mut self, item: T) {
+		if let Some(limit) = self.limit {
+			// If we have more committed operations than the limit, remove items from the committed
+			// list until we're at the limit. Then, remove one more to make space for the operation
+			// we're about to push.
+			while limit.get() <= self.committed.len() {
+				self.committed.pop_front();
+			}
+		}
+
+		self.committed.push_back(item);
+	}
+
+	/// [TODO: Description]
+	///
+	/// # Possible errors
+	/// * [`Error::NoApplicableHistory`] - No operations have been undone since the last time (if any)
+	///   queued operations were applied.
+	pub fn redo(&mut self) -> Result<&T, Error> {
+		// If there are no operations in the history, we have no work to do. Let the caller know.
+		if self.undone.is_empty() {
+			return Err(Error::NoApplicableHistory);
+		}
+
+		// Otherwise, pop an operation off the end of the undone list...
+		//
+		// NOTE: Because we just verified that `!self.undone.is_empty()`, we can safely assume
+		// this will be a `Some(_)` value.
+		let last_undone_operation = self.undone.pop().expect("undone list should not be empty");
+
+		// And add that item to the end of the committed list.
+		self.committed.push_back(last_undone_operation);
+
+		// Finally, return a reference to the item we just moved between list.
+		//
+		// NOTE: We unfortunately can't just return `&last_undone_operation`, as Rust seems to yell
+		// at us if we try.
+		let item_ref = self
+			.committed
+			.back()
+			.expect("committed list should not be empty");
+
+		Ok(item_ref)
+	}
+
+	/// [TODO: Description]
+	///
+	/// # Possible errors
+	/// * [`Error::NoApplicableHistory`] - There are no operations available to undo.
+	pub fn undo(&mut self) -> Result<&T, Error> {
+		// If there are no operations in the history, we have no work to do. Let the caller know.
+		if self.committed.is_empty() {
+			return Err(Error::NoApplicableHistory);
+		}
+
+		// Otherwise, pop an operation off the end of the history...
+		//
+		// NOTE: Because we just verified that `!self.committed.is_empty()`, we can safely assume
+		// this will be a `Some(_)` value.
+		let last_committed_operation = self
+			.committed
+			.pop_back()
+			.expect("committed list should not be empty");
+
+		// And add that item to the end of the undone list.
+		self.undone.push(last_committed_operation);
+
+		// Finally, return a reference to the item we just moved between list.
+		//
+		// NOTE: We unfortunately can't just return `&last_committed_operation`, as Rust seems to
+		// yell at us if we try.
+		let item_ref = self.undone.last().expect("undone list should not be empty");
+
+		Ok(item_ref)
+	}
+}
+
+// Manually impl Default, to avoid putting a bound on T.
+impl<T> Default for History<T> {
+	fn default() -> Self {
+		Self {
+			committed: VecDeque::new(),
+			undone: Vec::new(),
+			limit: None,
+		}
+	}
 }
 
 #[derive(Debug)]
