b58f174782f2b1d19c2f81913ee58a31b34bb3c3
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2024-12-24 03:59:46 -0500

    Avoid having the commands have similar code, by moving most of their code to a private function

--- src/extensions.rs
+++ src/extensions.rs
@@ -5,7 +5,7 @@ use bevy_ecs::{
 	world::{Command, Mut, World},
 };
 
-use crate::undoredo::UndoRedo;
+use crate::{error::Error as HistoryError, undoredo::UndoRedo};
 
 /// Extension trait for [`Commands`], adding undo/redo-related commands.
 pub trait CommandsUndoRedoExt {
@@ -13,50 +13,58 @@ pub trait CommandsUndoRedoExt {
 	/// resource.
 	///
 	/// # Panics
-	/// The command will panic if no [`UndoRedo`] has been registered as a resource.
+	/// The command will panic if no [`UndoRedo`] resource has been inserted.
 	fn undo(&mut self);
 	/// Pushes a [`Command`] to the queue for performing an redo using the world's [`UndoRedo`]
 	/// resource.
 	///
 	/// # Panics
-	/// The command will panic if no [`UndoRedo`] has been registered as a resource.
+	/// The command will panic if no [`UndoRedo`] resource has been inserted.
 	fn redo(&mut self);
 }
 
 impl CommandsUndoRedoExt for Commands<'_, '_> {
 	fn undo(&mut self) {
-		self.queue(UndoCommand);
+		self.queue(PerformUndo);
 	}
 
 	fn redo(&mut self) {
-		self.queue(RedoCommand);
+		self.queue(PerformRedo);
 	}
 }
 
-/// Performs an undo using the world's [`UndoRedo`] resource.
-struct UndoCommand;
+/// Grabs the `UndoRedo` resource from the world, creates a `Commands`, and then calls a given
+/// closure with both.
+///
+/// # Panics
+/// Panics if no [`UndoRedo`] resource has been inserted.
+fn use_undoredo_with_commands(
+	world: &mut World,
+	f: impl FnOnce(&mut UndoRedo, &mut Commands) -> Result<(), HistoryError>,
+) -> Result<(), HistoryError> {
+	// We have to use a resource scope here, as we also need to create a new `Commands` - but
+	// attempting to do so while UndoRedo is still in the World would result in us violating
+	// Rust's aliasing rules.
+	world.resource_scope(|world, mut undoredo: Mut<UndoRedo>| {
+		let mut commands = world.commands();
+		f(&mut undoredo, &mut commands)
+	})
+}
 
-impl Command for UndoCommand {
+/// Command that performs an undo using the world's [`UndoRedo`] resource.
+pub struct PerformUndo;
+
+impl Command for PerformUndo {
 	fn apply(self, world: &mut World) {
-		world.resource_scope(|world, mut undoredo: Mut<UndoRedo>| {
-			let mut commands = world.commands();
-			// We intentionally ignore the result.
-			// Though perhaps in the future we could write an event with the result?
-			let _ = undoredo.undo(&mut commands);
-		});
+		let _ = self::use_undoredo_with_commands(world, UndoRedo::undo);
 	}
 }
 
-/// Performs a redo using the world's [`UndoRedo`] resource.
-struct RedoCommand;
+/// Command that performs a redo using the world's [`UndoRedo`] resource.
+pub struct PerformRedo;
 
-impl Command for RedoCommand {
+impl Command for PerformRedo {
 	fn apply(self, world: &mut World) {
-		world.resource_scope(|world, mut undoredo: Mut<UndoRedo>| {
-			let mut commands = world.commands();
-			// We intentionally ignore the result.
-			// Though perhaps in the future we could write an event with the result?
-			let _ = undoredo.redo(&mut commands);
-		});
+		let _ = self::use_undoredo_with_commands(world, UndoRedo::redo);
 	}
 }
